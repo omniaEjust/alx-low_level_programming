a622b762568065d8468ba3b9a888b54e616688d4
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-23 12:59:48 -0700

    my fist commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 100 == 0 || year % 400 == 0)
+	/**
+	 * leap year when it can be divided
+	 * by 100 and 400 or by 4 evenly
+	 */
+	if ((year % 100 == 0 || year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -30,7 +34,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
