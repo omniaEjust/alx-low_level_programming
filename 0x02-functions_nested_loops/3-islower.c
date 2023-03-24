4955102f702b15b0d09f7d8cf5ed64949af509f7
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-24 08:31:13 -0700

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * islower - a function that checks for lowercase character
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise always 0 (success)
+*/
+
+int _islower(int c)
+{
+	for (ch >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
