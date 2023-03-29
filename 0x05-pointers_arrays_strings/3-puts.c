bb56b5dbf5db1107c61a05998b9164fd8f89a7dd
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-29 15:15:13 -0700

    a

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string,
- * followed by a new line, to stdout
+ * _puts - prints a string, followed by a newline to stdout
  *
  * @str: string parameter to print
  *
