9293a707f9e5babd6aa54b4e61ccdc336892b68d
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-31 06:15:57 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
 /**
  * puts_half - a function that prints the second half of a string
  *
- * @str: string parameter input
+ * @str: char array string type
  *
  * Description: if odd number of chars, print (length - 1) / 2
  */
