c6a55545c8a3eab18c7b5a7d7f3c2bd1f4427397
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-31 05:54:47 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints the second half of a string
+ *
+ * @str: string parameter input
+ *
+ * Description: if odd number of chars, print (length - 1) / 2
+ */
+
+void puts_half(char *str)
+{
+	int i
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
