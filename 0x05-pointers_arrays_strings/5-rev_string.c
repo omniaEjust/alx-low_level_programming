8b876b805857425e2bbb18fc9b696acbec6f45a1
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-31 05:27:16 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,13 +12,15 @@ void rev_string(char *s)
 {
 	int l, i;
 	char temp;
-
+	
+	/*find string length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
 		;
+	/*swap the string by looping to half the string*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[l - 1 - i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
 		s[l - 1 - i] = temp;
 	}
 }
