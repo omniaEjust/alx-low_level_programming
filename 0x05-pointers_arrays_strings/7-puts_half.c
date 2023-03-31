b9de8bac119e2ba6bb013cb93cdb872b7aeb8f6c
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-31 06:45:04 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,13 @@ void puts_half(char *str)
 {
 	int j = 0;
 	int k;
-	
+
 	while (str[j] != '\0')
 		j++;
 	if (j % 2 == 1)
 	{
-		k = (j - 1) / 2;  k += 1;
+		k = (j - 1) / 2;
+		k += 1;
 	}
 	else
 		k = j / 2;
