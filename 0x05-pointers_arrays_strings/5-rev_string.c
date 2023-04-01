95073cdc4cc9d68b0a7a7ab29a188b1a6d3b77e3
omniaEjust <omnia.ghonem@ejust.edu.eg>
2023-03-31 19:45:59 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,15 @@
 
 void rev_string(char *s)
 {
-	int l, i;
-	char temp;
+	int len = 0, index = 0;
+	char tmp;
 
-	for (l = 0; s[l] != '\0'; ++l)
-		;
-	for (i = 0; i < 1 / 2; i++)
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		temp = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = temp;
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
 }
